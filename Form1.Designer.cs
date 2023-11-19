6de821d900051b0688eadbfc2c704b31273a3937
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-20 00:14:11 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace tekmi_ciftmi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnYAzdir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(125, 31);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "bir sayı giriniz";
+            // 
+            // btnYAzdir
+            // 
+            this.btnYAzdir.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnYAzdir.Location = new System.Drawing.Point(59, 120);
+            this.btnYAzdir.Name = "btnYAzdir";
+            this.btnYAzdir.Size = new System.Drawing.Size(75, 23);
+            this.btnYAzdir.TabIndex = 2;
+            this.btnYAzdir.Text = "yazdır";
+            this.btnYAzdir.UseVisualStyleBackColor = false;
+            this.btnYAzdir.Click += new System.EventHandler(this.btnYAzdir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(426, 194);
+            this.Controls.Add(this.btnYAzdir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnYAzdir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekmi_ciftmi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYAzdir_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(txtSayi.Text);
+            if(sayi%2 == 0 )
+            {
+                MessageBox.Show("sayınız çifttir");
+            }else if(sayi%2 == 1 )
+            {
+                MessageBox.Show("sayınız tektir");
+
+            }
+            else
+            {
+                MessageBox.Show("sayınız sıffırdır");
+            }
+        }
+    }
+}
